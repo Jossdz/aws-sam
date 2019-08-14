41059a9c5957950abfd42047c679b0a34aaff563
jossdz <carloscorrea.dev@gmail.com>
2019-08-14 08:00:41 -0500

    hello aws-sam

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import useState and useEffect hooks from React
+import React, {useState, useEffect} from 'react'
+
+// import the API category from AWS Amplify
+import {API} from 'aws-amplify'
+
+import './App.css'
 
 function App() {
+  // create coins variable and set to empty array
+  const [coins, updateCoins] = useState([])
+
+  // define function to all API
+  async function fetchCoins() {
+    const data = await API.get('cryptoapi', '/coins')
+    updateCoins(data.coins)
+  }
+
+  // call fetchCoins function when component loads
+  useEffect(() => {
+    fetchCoins()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {coins.map((coin, index) => (
+        <div key={index}>
+          <h2>
+            {coin.name} - {coin.symbol}
+          </h2>
+          <h5>${coin.price_usd}</h5>
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
